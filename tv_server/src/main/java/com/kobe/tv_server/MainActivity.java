28e2608ca19806a9b1babb02755ff1b1a788fad0
chenjialei <cjlkbxt@163.com>
2020-08-27 10:07:40 +0800

    支持4.4

--- tv_server/src/main/java/com/kobe/tv_server/MainActivity.java
+++ tv_server/src/main/java/com/kobe/tv_server/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
